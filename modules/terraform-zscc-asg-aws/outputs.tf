6b8052df2cf93832caa9ab39fc04d12b53ffcd03
Zoltan Kovacs <zoltan@ikovacs.com>
2022-12-09 13:18:30 -0600

    Initial Commit

--- modules/terraform-zscc-asg-aws/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "availability_zone" {
-  description = "Availability zones used for ASG"
-  value       = aws_autoscaling_group.cc_asg.availability_zones
-}
--- modules/terraform-zscc-iam-aws/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "iam_instance_profile_id" {
-  description = "IAM Instance Profile Name"
-  value       = data.aws_iam_instance_profile.cc_host_profile_selected[*].name
-}
-
-output "iam_instance_profile_arn" {
-  description = "IAM Instance Profile ARN"
-  value       = data.aws_iam_instance_profile.cc_host_profile_selected[*].arn
-}
